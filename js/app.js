2d3af442361e8c12e474e8ec818979a5481b774d
Laura Padilla <hi@lauracodes.com>
2020-12-07 21:27:17 -0500

    update font in app.js file

--- js/app.js
+++ js/app.js
@@ -1,75 +1,62 @@
-let font
-let graphic
+let font;
+let graphic;
 
 function preload() {
-  font = loadFont('standard-bold.otf')
+  font = loadFont("fonts/standard-bold.otf");
 }
 
 function setup() {
-  createCanvas(1000, 600)
+  createCanvas(1000, 600);
 
-  graphic = createGraphics(1000, 600)
+  graphic = createGraphics(1000, 600);
 
-  graphic.fill('#ff330c')
-  graphic.textFont(font)
-  graphic.textSize(200)
-  graphic.textAlign(CENTER, CENTER)
-  graphic.text('Dope HipHop', 450, 200, 200, 300)
+  graphic.fill("#ff330c");
+  graphic.textFont(font);
+  graphic.textSize(200);
+  graphic.textAlign(CENTER, CENTER);
+  graphic.text("Dope HipHop", 450, 200, 200, 300);
 }
 
 function draw() {
-  background("#fff")
-  
-  const tileSize = 40
-  
+  background("#fff");
+
+  const tileSize = 40;
+
   for (let x = 0; x < 25; x = x + 1) {
     for (let y = 0; y < 25; y = y + 1) {
-      
-      const distortion = sin(frameCount * 0.05 + x * 0.5 + y * 0.3) * 25
-      
-      // source
-      const sx = x * tileSize + distortion
-      const sy = y * tileSize
-      const sw = tileSize
-      const sh = tileSize
-      
-      //destination
-      const dx = x * tileSize 
-      const dy = y * tileSize
-      const dw = tileSize 
-      const dh = tileSize
+      const distortion = sin(frameCount * 0.05 + x * 0.5 + y * 0.3) * 25;
 
-      image(graphic, dx, dy, dw, dh, sx, sy, sw, sh)
+      // source
+      const sx = x * tileSize + distortion;
+      const sy = y * tileSize;
+      const sw = tileSize;
+      const sh = tileSize;
+
+      //destination
+      const dx = x * tileSize;
+      const dy = y * tileSize;
+      const dw = tileSize;
+      const dh = tileSize;
+
+      image(graphic, dx, dy, dw, dh, sx, sy, sw, sh);
     }
   }
 }
 
 // Fade-in images
-inView('.fade')
-  .on('enter', img => img.classList.add('visible'))
-  .on('exit', img => img.classList.remove('visible'))
-
+inView(".fade")
+  .on("enter", (img) => img.classList.add("visible"))
+  .on("exit", (img) => img.classList.remove("visible"));
 
 // About modal
-const aboutLink = document.querySelector(".about")
-const aboutModal = document.querySelector(".about-modal")
-const close = document.querySelector(".close")
+const aboutLink = document.querySelector(".about");
+const aboutModal = document.querySelector(".about-modal");
+const close = document.querySelector(".close");
 
 aboutLink.addEventListener("click", function () {
-  aboutModal.classList.remove("d-none")
-})
+  aboutModal.classList.remove("d-none");
+});
 
 close.addEventListener("click", function () {
-  aboutModal.classList.add("d-none")
-})
-
-
-
-
-
-
-
-
-
-
-
+  aboutModal.classList.add("d-none");
+});
